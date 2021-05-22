d95ddd926b9630b38fabece3636fad1a1cbd1356
Max Stanley <git@maxstanley.uk>
2021-05-22 15:10:05 +0100

    fixed home-manager

--- profiles/personal.nix
+++ profiles/personal.nix
@@ -1,7 +1,6 @@
 { config, pkgs, lib, ... }: {
   imports = [
     ./common.nix
-    ../users/max/base.nix
     ../users/max/personal.nix
   ];
 }
--- users/max/personal.nix
+++ users/max/personal.nix
@@ -1,16 +1,57 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }:
+let
+  home-manager = builtins.fetchGit {
+    url = "https://github.com/rycee/home-manager.git";
+    rev = "2aa20ae969f2597c4df10a094440a66e9d7f8c86";
+    ref = "release-20.09";
+  };
+in {
   imports = [
-    <home-manager/nixos>
+    # <home-manager/nixos>
+    (import "${home-manager}/nixos")
   ];
 
-  home-manager.users.max = { pkgs, ... }: {
-    programs = {
+  users.extraUsers.max = {
+    isNormalUser = true;
+    extraGroups = lib.mkDefault [
+      "wheel"
+    ];
+  };
 
-       git = {
-         userName = "Max Stanley";
-         userEmail = "git@maxstanley.uk";
-       };
-           
-     };
-   };
+  home-manager = {
+    useUserPackages = true;
+    useGlobalPkgs = true;
+
+    users.max = { pkgs, ... }: {
+      programs = {
+
+        zsh = {
+	  enable = true;
+
+          history = {
+              size = 1000;
+	      save = 1000;
+	      path = "$HOME/.zsh_history";
+	      share = true;
+	  };
+
+#	  histSize = 1000;
+#	  setOptions = [
+#
+#	  ];
+	};
+
+	ssh = {
+	  enable = true;
+	};
+
+	git = {
+	  enable = true;
+          userName = "Max Stanley";
+          userEmail = "git@maxstanley.uk";
+	};
+	    
+      };
+    };
+  };
 }
