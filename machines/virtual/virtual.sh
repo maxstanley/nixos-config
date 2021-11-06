a378fe2285df578d9a1a5f3276dc994c28a2e31a
Max Stanley <github@maxstanley.uk>
2021-11-06 18:48:45 +0000

    Force filesystem creation

--- machines/virtual/virtual.sh
+++ machines/virtual/virtual.sh
@@ -7,7 +7,7 @@ parted --script /dev/sda -- \
     mkpart primary linux-swap -8GiB 100%
 
 # https://nixos.org/manual/nixos/stable/#sec-installation-summary
-mkfs.ext4 -L nixos /dev/sda1
+mkfs.ext4 -F -L nixos /dev/sda1
 mkswap -L swap /dev/sda2
 swapon /dev/sda2
 mount /dev/disk/by-label/nixos /mnt
